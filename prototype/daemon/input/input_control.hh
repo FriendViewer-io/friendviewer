b9039ba5daf18faa80bbbf7a55789812d2f5c5f0
shiiion <mayansymbol@gmail.com>
2019-06-30 22:49:07 -0700

    Created input controller, added user32 dependency

---
+++ prototype/daemon/input/input_control.hh
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace prototype {
+namespace daemon {
+namespace input {
+
+void set_cursor_location(float x, float y);
+void send_virtual_input(int virtual_key);
+
+}  // namespace input
+}  // namespace daemon
+}  // namespace prototype
