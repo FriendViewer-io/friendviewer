e7090dcbb0f68d16b8d5e0e714467c03832b884d
JoeCleary <joseph.david.cleary@gmail.com>
2019-07-01 23:04:35 -0400

    java bazel works

--- prototype/distributor/src/ClientHandler.java
+++ prototype/distributor/src/ClientHandler.java
@@ -10,35 +10,15 @@ import java.util.Scanner;
 
 public class ClientHandler extends ChannelInboundHandlerAdapter {
 
-    /*public int counter = 0;
-    public ChannelHandlerContext context = null;
-
-    @Override
-    public void handlerAdded(ChannelHandlerContext ctx) {
-        context = ctx;
-    }
-    
-    public void initiateConvo(int x){
-        final ByteBuf time = context.alloc().buffer(4); // (2)
-        time.writeInt(x);
-
-        final ChannelFuture f = context.writeAndFlush(time); // (3)
-    }*/
-
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("client recieved");//debug code
-
         String reply = scanner.nextLine();
 
         byte[] bytes = reply.getBytes();
         ByteBuf text = ctx.alloc().buffer(bytes.length);
         text.writeBytes(bytes);
         ctx.writeAndFlush(text);
-
-        //text.release();
     }
 
     @Override
